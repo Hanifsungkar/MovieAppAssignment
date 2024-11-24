6713347eb4b031bdb127fa2842faab6f03a1c583
Hanif Sungkar <hanif.sungkar1@gmail.com>
2024-11-24 15:14:44 +0700

    Commit2

---
+++ assignment6/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './Style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from './components/MovieList';
+import MovieHeading from './components/MovieHeading';
+import SearchBox from './components/SearchBox';
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('')
+
+  const getMovieReq = async () => {
+    const url = `http://www.omdbapi.com/?s=${search}&apikey=${process.env.REACT_APP_API_KEY}`;
+    
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+   if(responseJson.Search) {
+    setMovies(responseJson.Search);
+   }
+  }
+
+  useEffect(() => {
+    getMovieReq(search);
+  }, [search])
+
+  return (
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieHeading heading ='MOVIES'/>
+        <SearchBox search={search} setSearch={setSearch} />
+      </div>
+      <div className='row'>
+        <MovieList movies = {movies} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
